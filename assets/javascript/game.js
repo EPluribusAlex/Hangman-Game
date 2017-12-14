021f7034948576b4aaaad6c3378534cdbc422066
AlexFP <florianprescott@googlemail.com>
2017-12-14 14:50:43 -0500

    basic hangman code in place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,96 @@
-var wins = 0;
-var guessesLeft = 13;
-var userGuessTally = [];
-var gameWordList = ["cybernetic", "organism", "computer", "intelligence", "system", "framework"];
+window.onload = function () {
+	//declaring variables
+	var wins = 0;
+	var guessesLeft = 13;
+	var userGuessTally = [];
+	var gameWordList = ["cybernetic", "organism", "computer", "intelligence", "system", "framework"];
+	//showing starting values
+	document.getElementById('winCounter').innerHTML = wins; 
+	document.getElementById('guessesLeft').innerHTML = guessesLeft; 
+	//key-press begins the first round
+	document.onkeypress = function gameLoop(event) {
+		//generate a word to guess and an array of the letters that word comprises
+		var gameWord = gameWordList[Math.floor(Math.random() * gameWordList.length)];
+		var letters = Array.from(gameWord);
 
-document.getElementById('winCounter').innerHTML = wins; 
-document.getElementById('guessesLeft').innerHTML = guessesLeft;  
+		var para = document.createElement('P');
 
-document.onkeypress = function(event) {
-	var gameWord = gameWordList[Math.floor(Math.random() * gameWordList.length)];
+		document.getElementById('note').innerHTML = "Type Letters on the Kyboard to Guess!";
 
-	console.log(gameWord);
-	console.log(wins);
-	console.log(guessesLeft);
-	console.log(userGuessTally);
+		for(var i = 0; i < letters.length; i++) {
+			var t = document.createTextNode("_");
+			para.appendChild(t);
+		}
+		document.getElementById('currentWord').appendChild(para);
 
-	document.onkeypress = function(event) {
-		var userGuess = event.key;
+		var letterSpace = document.getElementById('currentWord').textContent;
+		var revealedLetters = Array.from(letterSpace);
 
-		if(gameWord.includes(userGuess)) {
-			
-		} 
+		console.log(letters);
+		console.log(gameWord);
+		console.log(wins);
+		console.log(guessesLeft);
+		console.log(userGuessTally);
+		console.log(letterSpace);
+		console.log(revealedLetters);
 
-		else {
-			guessesLeft--;
-			document.getElementById('guessesLeft').innerHTML = guessesLeft; 
+		document.onkeypress = function(event) {
+			var userGuess = event.key;
 
-			if(guessesLeft == 0) {
-				losses++;
-				guessesLeft = 9;
-				userGuessTally.length = 0;
-				gameLetter = alphabet[Math.floor(Math.random() * alphabet.length)]; 
+			console.log(userGuess);
+
+			if(gameWord.includes(userGuess)) {
+				for(var i = 0; i < letters.length; i++) {
+					if(userGuess === letters[i]) {
+							revealedLetters.splice(i, 1, userGuess);
+					}
+				}
+
+				console.log(revealedLetters);
+				document.getElementById('currentWord').innerHTML = revealedLetters.join("");
+
+				if(revealedLetters.includes("_")) {
+
+				}
+				else {
+					document.getElementById('note').innerHTML = "You got it!  Press any key to play again.";
+					wins++;
+					document.getElementById('winCounter').innerHTML = wins;
+					document.onkeypress = function(event) {
+						revealedLetters.length = 0;
+						document.getElementById('currentWord').innerHTML = "";
+						userGuessTally.length = 0;
+						document.getElementById('lettersTried').innerHTML = "";
+						guessesLeft = 13;
+						document.getElementById('guessesLeft').innerHTML = guessesLeft;
+						gameLoop();
+					}
+				}
+
 			} 
 
 			else {
-				userGuessTally.push(" " + userGuess);
-			};
-		};
-	};
+				guessesLeft--;
+				document.getElementById('guessesLeft').innerHTML = guessesLeft; 
+
+				if(guessesLeft == 0) {
+					document.getElementById('note').innerHTML = "Sorry, Try Again!";
+					document.onkeypress = function(event) {
+						revealedLetters.length = 0;
+						document.getElementById('currentWord').innerHTML = "";
+						userGuessTally.length = 0;
+						document.getElementById('lettersTried').innerHTML = ""; 
+						guessesLeft = 13;
+						document.getElementById('guessesLeft').innerHTML = guessesLeft;
+						gameLoop();
+					}
+				} 
+
+				else {
+					userGuessTally.push(" " + userGuess);
+					document.getElementById('lettersTried').innerHTML = userGuessTally;
+				}
+			}
+		}
+	}
 }
